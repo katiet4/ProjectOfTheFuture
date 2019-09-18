2e3e48f0d78e0026f2e87d4a14f1b3aa3cc86686
katiet4 <anton_16_78_01_01@mail.ru>
2019-09-18 22:00:17 +0500

    .

--- ProjectOfTheFuture/Login/models.py
+++ ProjectOfTheFuture/Login/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+	FirstName = models.CharField(max_length=50)
+	LastName  = models.CharField(max_length=50)
+	NickName  = models.CharField(max_length=50)
+
+	Sex       = models.CharField(max_length=1)
+
+	bYear     = models.IntegerField()
+	bMonth    = models.IntegerField()
+	bDay      = models.IntegerField()
+
+	dateReg   = models.DateTimeField()
+
+	def __str__(self):
+		return self.NickName
--- ProjectOfTheFuture/Login/views.py
+++ ProjectOfTheFuture/Login/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def LoginDef(request):
+
+	if request.POST:
+		pass
+
+	return render(request, "index.html")
