5802b5947b72a2dff49b4a29e84724dc96263d18
katiet4 <anton_16_78_01_01@mail.ru>
2019-09-19 01:37:38 +0500

    '.'

--- ProjectOfTheFuture/Login/views.py
+++ ProjectOfTheFuture/Login/views.py
@@ -4,6 +4,6 @@ def LoginDef(request):
 
 	if request.POST:
 		email = request.POST.get('email')
-		password  = request.POST.get('pass')
+		#password  = request.POST.get('pass')
 
 	return render(request, "index.html")
