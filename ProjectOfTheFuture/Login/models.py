c6140dcad3842efc9b9d46954d252734c0ed0ce6
katiet4 <anton_16_78_01_01@mail.ru>
2019-09-19 22:15:20 +0500

    '.'

--- ProjectOfTheFuture/Login/models.py
+++ ProjectOfTheFuture/Login/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime  
 
 class Users(models.Model):
 	FirstName = models.CharField(max_length=50)
@@ -15,7 +15,16 @@ class Users(models.Model):
 	bMonth    = models.IntegerField()
 	bDay      = models.IntegerField()
 
-	dateReg   = models.DateTimeField()
+	dateReg   = models.DateTimeField(default=datetime.now(), blank=True)
 
+	#profile
+	status = models.TextField(default = "0")
+	rank =  models.TextField(default = '1')
+	title = models.TextField(default = 'NoNe')
+	language =  models.TextField(default = 'NoNe')
+	country =  models.TextField(default = 'NoNe')
+	city =  models.TextField(default = 'NoNe')
+	telephone =  models.TextField(default = '0')
+	groups =  models.TextField(default = 'user')
 	def __str__(self):
 		return self.NickName
--- ProjectOfTheFuture/Register/views.py
+++ ProjectOfTheFuture/Register/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 
 def register(request):
+	if request.POST:
+		email = request.POST.get('index_email')
+		password  = request.POST.get('index_pass')
+		nick  = request.POST.get('NickName')
+		Fname = request.POST.get('FirstName')
+		Lname = request.POST.get('LastName')
+		sex = request.POST.get('sex')
+		Bday = request.POST.get('day')
+		Bmonth = request.POST.get('month')
+		Byear = request.POST.get('year')
+		print(email)
+		print(password)
+		print(nick)
+		print(Fname)
+		print(Lname)
+		print(sex)
+		print(Bday)
+		print(Bmonth)
+		print(Byear)
 	return render(request, 'register.html')
+
