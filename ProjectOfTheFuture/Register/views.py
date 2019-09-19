443bc3357fb982d80e0f3b39d00291e48122f5f5
katiet4 <anton_16_78_01_01@mail.ru>
2019-09-19 22:36:36 +0500

    '.'

--- ProjectOfTheFuture/Register/views.py
+++ ProjectOfTheFuture/Register/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from Login.models import Users
 def register(request):
 	if request.POST:
 		email = request.POST.get('index_email')
@@ -8,17 +8,22 @@ def register(request):
 		Fname = request.POST.get('FirstName')
 		Lname = request.POST.get('LastName')
 		sex = request.POST.get('sex')
-		Bday = request.POST.get('day')
-		Bmonth = request.POST.get('month')
-		Byear = request.POST.get('year')
-		print(email)
-		print(password)
-		print(nick)
-		print(Fname)
-		print(Lname)
-		print(sex)
-		print(Bday)
-		print(Bmonth)
-		print(Byear)
+		BDay = request.POST.get('day')
+		BMonth = request.POST.get('month')
+		BYear = request.POST.get('year')
+		user = Users(FirstName = Fname, LastName = Lname, NickName = nick, 
+						email = email, password = password, Sex = 'M' if sex == 'Male' else "F",
+						bYear = BYear, bMonth = BMonth, bDay = BDay)
+		user.save()
+
+		# print(email)
+		# print(password)
+		# print(nick)
+		# print(Fname)
+		# print(Lname)
+		# print(sex)
+		# print(Bday)
+		# print(Bmonth)
+		# print(Byear)
 	return render(request, 'register.html')
 
