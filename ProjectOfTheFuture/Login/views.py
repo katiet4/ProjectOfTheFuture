f220650262b94ddfc7ece9e4926fdd185b1e6b47
katiet4 <anton_16_78_01_01@mail.ru>
2019-09-23 23:52:50 +0500

    '.'.

--- ProjectOfTheFuture/Login/views.py
+++ ProjectOfTheFuture/Login/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from Login.models import Users
 def LoginDef(request):
-
 	if request.POST:
 		email = request.POST.get('email')
-		#password  = request.POST.get('pass')
+		password  = request.POST.get('pass')
+		if Users.objects.filter(email = email).exists():
+			return render(request, 'index.html', {'Answer':'Error'})
+		if Users.objects.get(email = email).password == password:
+			request.session['Aunt'] = "TrueAunt"
+			#переход на страницу
+		
 
 	return render(request, "index.html")
